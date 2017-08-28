ab34aa1272e7661bd4a956d63d8d0140380a5c31
Kendrick <kendrick@media.figaro.fr>
2017-08-28 17:12:13 +0200

    fix import BadRequestHttpException missing

--- src/AppBundle/Controller/OtpController.php
+++ src/AppBundle/Controller/OtpController.php
@@ -10,6 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class OtpController extends Controller
 {
